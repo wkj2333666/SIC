8d10baffaf2bf5232067fc4e6e8db45556f6618e
wkj2333666 <2695071759@qq.com>
2025-05-13 15:49:49 +0800

    5.13

--- src/Calculator.h
+++ src/Calculator.h
@@ -6,15 +6,16 @@ class Calculator {
     std::vector<std::shared_ptr<BaseToken>> postfix;
     std::vector<std::shared_ptr<BaseToken>> stack;
     Tokenizer* tokenizer;
+
+    void tokenize(const std::string& input);
+    void transform();
+    std::shared_ptr<DataToken> calculate();
+    void pop_until_LPAR();
 public:
     Calculator(): tokens(), postfix(), stack(), tokenizer(new Tokenizer()) {}
     ~Calculator() {
         delete tokenizer;
     }
-    void tokenize(const std::string& input);
-    void transform();
-    std::shared_ptr<DataToken> calculate();
     void run(const std::string& input);
-    void pop_until_LPAR();
     void clear();
 };
